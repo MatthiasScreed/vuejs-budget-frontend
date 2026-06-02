8e8c6c7158263ad7a225a740b9788e30afbb54c1
Christopher MASSAMBA <christopher.massamba@cegelem.fr>
2026-06-02 14:31:15 +0200

    MVP/Login~4

--- src/stores/Dailyactionstore.ts
+++ src/stores/Dailyactionstore.ts
@@ -3,7 +3,7 @@ import { ref, computed } from 'vue'
 import { api } from '@/services/api'
 import { useToastStore } from '@/stores/toastStore'
 import { useGamingStore } from '@/stores/gamingStore'
-import { useQuestStore } from '@/stores/QuestStore'
+import { useQuestStore } from '@/stores/Queststore.ts'
 
 // ==========================================
 // TYPES
